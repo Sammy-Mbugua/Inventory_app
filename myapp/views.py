361f0cc299dab5c31fea2ff77e495b798910c3b6
Sammy-Mbugua <msammy542@gmail.com>
2026-01-09 03:35:28 +0300

    init:creating_urls path

--- myapp/views.py
+++ myapp/views.py
@@ -1,46 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Products
 from .forms import ProductForm
 
 # Create your views here.
-def product_list(request):
+def product_list_view(request):
     products = Products.objects.all()
     return render(request, 'product_list.html', {'products': products})
 
-def add_product(request):
+def product_create_view(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, 'product_success.html')
-    else:
-        form = ProductForm()
-    return render(request, 'add_product.html', {'form': form})
+            return redirect(request, 'product_list.html')
+    return redirect(request, 'product_form.html', {'form': form})
 
-def product_detail(request, product_id):
-    product = Products.objects.get(product_id=product_id)
-    return render(request, 'product_detail.html', {'product': product})
-
-def edit_product(request, product_id):
+def product_update_view(request, product_id):
     product = Products.objects.get(product_id=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, instance=product)
         if form.is_valid():
             form.save()
-            return render(request, 'product_success.html')
-    else:
-        form = ProductForm(instance=product)
-    return render(request, 'edit_product.html', {'form': form, 'product': product})
+            return redirect(request, 'product_list.html')
+    return render(request, 'product_form.html', {'form': form, 'product': product})
 
-def delete_product(request, product_id):
+def product_delete_view(request, product_id):
     product = Products.objects.get(product_id=product_id)
     if request.method == 'POST':
         product.delete()
-        return render(request, 'product_success.html')
+        return redirect(request, 'product_list.html')
     return render(request, 'delete_product.html', {'product': product})
 
 def home(request):
     return render(request, 'home.html')
+
+
+
 
 
 
